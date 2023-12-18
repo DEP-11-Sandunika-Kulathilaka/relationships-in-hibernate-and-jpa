f796d6b299198f8bd256fbb4e21b75ca6c6922a1
Sadunikaku <kulathilakasandunika@gmail.com>
2023-12-18 23:09:33 +0530

    Create Employee and Spouse Tables

--- src/main/java/lk/ijse/dep11/app/OneToOneDemo1.java
+++ src/main/java/lk/ijse/dep11/app/OneToOneDemo1.java
@@ -1,2 +1,24 @@
-package lk.ijse.dep11.app;public class OneToOneDemo1 {
+package lk.ijse.dep11.app;
+
+import lk.ijse.dep11.app.db.HibernateUtil;
+import org.hibernate.*;
+
+public class HelloHibernate {
+    public static void main(String[] args) {
+        try (SessionFactory sf = HibernateUtil.getSessionFactory();
+             Session session = sf.openSession()) {
+            Transaction tx = session.beginTransaction();
+
+            try {
+                System.out.println(session);
+                tx.commit();
+            } catch (Throwable t) {
+                tx.rollback();
+                throw t;
+            } finally {
+                session.close();
+                sf.close();
+            }
+        }
+    }
 }
